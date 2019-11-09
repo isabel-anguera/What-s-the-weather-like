866e0f1e7792cd226b414fa791d2144688fec14c
Isabel Anguera <isabelanguera2@gmail.com>
2019-11-09 18:23:16 -0500

    minorChanges

--- api-index.js
+++ api-index.js
@@ -1,10 +1,17 @@
+var input = document.getElementById("text-box");
+const url = 'http://api.openweathermap.org/data/2.5/weather?q=New York&APPID=3f56d66bf44e9bbf90cbf9c5a0db23a5';
+var userInput = document.getElementById("text-box").value;
+console.log(userInput);
+
+var searchedCities = [];
+
 console.log('Hello, Sheet!');
 
 window.onload = requestJSON;
 setInterval(requestJSON, 10000);
 
 function requestJSON() {
-  fetch('http://api.openweathermap.org/data/2.5/weather?q=New%20York&APPID=3f56d66bf44e9bbf90cbf9c5a0db23a5')
+  fetch(url)
     .then(response => response.json())
     .then(gotJSON);
 }
@@ -12,6 +19,7 @@ function requestJSON() {
 function gotJSON(json) {
   console.log('Update Data');
   const weatherInfo = json.weather;
+  // const mainInfo = json.main;
 
   const answer = document.getElementById('answer');
 
@@ -24,5 +32,36 @@ function gotJSON(json) {
     const newH3 = document.createElement('h3');
     newH3.innerHTML = desc;
     answer.appendChild(newH3);
+   }
   }
-}
+
+  function searchCity() {
+    console.log('hey');
+    searchedCities.push(userInput);
+    userInput.textContent += searchedCities;
+    input.textContent = userInput;
+    console.log(userInput);
+    console.log(searchedCities);
+    console.log(input);
+
+
+  }
+
+  // for (const main of mainInfo) {
+  //   const temperature = main.temp;
+  //   console.log(temperature);
+
+  //   const newH3 = document.createElement('h3');
+  //   newH3.innerHTML = temperature - 273.15*9/5 + 32 + "°F";
+  //   answer.appenChild(newH3);
+  // }
+
+
+// input.addEventListener("keyup", function(event) {
+//   if (event.keyCode === 13) {
+//    event.preventDefault();
+//    }
+//  }
+
+
+
